fa70698dce227637897bf6fae13a2722bfb34ba8
shuaizhang621 <sz1950@nyu.edu>
2018-03-24 18:00:43 -0400

    login and router

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { WrappedRegister } from './Register';
+import { Login } from './Login';
 
 export class Main extends React.Component {
     render() {
         return (
             <section className="main">
-                <WrappedRegister/>
+                <Login/>
             </section>
         )
     }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BroweserRouter>
+        <App />
+    </BroweserRouter>
+    , document.getElementById('root'));
 registerServiceWorker();
