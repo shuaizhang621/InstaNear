dc1f29395c9c852dac6cf879fa7de00400878248
shuaizhang621 <sz1950@nyu.edu>
2018-03-24 19:24:09 -0400

    redirect

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import { Register } from './Register';
 import { Login } from './Login';
-import { Switch, Route } from 'react-router'
+import { Switch, Route, Redirect } from 'react-router'
+import { Home } from './Home';
 
 
 export class Main extends React.Component {
+    getLogin = () => {
+        return this.props.isLoggedIn ? <Redirect to="/home"/> : <Login handleLogin={this.props.handleLogin}/>;
+    }
+
+    getHome = () => {
+        return this.props.isLoggedIn ? <Home/> : <Redirect to="/login"/>;
+    }
+
+    getRoot = () => {
+        return <Redirect to="/login"/>;
+    }
+
     render() {
         return (
             <div className="main">
                 <Switch>
-                    <Route exact path="/" component={Login}/>
-                    <Route path="/login" component={Login}/>
+                    <Route exact path="/" render={this.getRoot}/>
+                    <Route path="/login" render={this.getLogin}/>
                     <Route path="/register" component={Register}/>
-                    <Route component={Login}/>
+                    <Route path="/home" render={this.getHome}/>
+                    <Route render={this.getRoot}/>
                 </Switch>
             </div>
         )
